61ae5c4d20ef62eab4fcb3eb4ed900f7d581b7f2
bastidas6 <manuba2309@gmail.com>
2019-09-29 17:09:56 -0500

    last commit

--- consumo.js
+++ consumo.js
@@ -1,9 +1,26 @@
 
-function diHola(){ //Funcion aplicable a un boton para decir hola
-    alert('Holaa!');
-}
-
-function diChao(){
-    alert('Chaolin pinguin');
+document.querySelector('#dolar').addEventListener('click', function(){
+    obtenerDatos('dolar');
+});
+document.querySelector('#uf').addEventListener('click', function(){
+    obtenerDatos('uf');
+});
+ 
+function obtenerDatos(valor){
+    let url = `https://mindicador.cl/api/${valor}`;
+    const api = new XMLHttpRequest();
+    api.open('GET',url, true);
+    api.send();
+    api.onreadystatechange = function(){
+        if(this.status == 200 && this.readyState == 4){
+            let datos = JSON.parse( this.responseText);
+            console.log(datos.serie)
+            let resultado = document.querySelector('#resultado');
+            resultado.innerHTML = '';
+            for(let item of datos.serie){
+                resultado.innerHTML += `<li>${item.fecha.substr(0,10)} | ${item.valor}<li>`;
+            }
+        }
+    }
 }
 
